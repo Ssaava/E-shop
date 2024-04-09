aacf0728ad3d43b8d2584856bc8c8a4982ca0008
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2024-04-09 13:29:42 +0300

    page not found

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,31 +1,37 @@
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Icon from "@mui/material/Icon";
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import ListItemText from "@mui/material/ListItemText";
-import ItemCard from "../../components/ItemCard/ItemCard";
+import Typography from "@mui/material/Typography";
+import ItemCard from "../../components/PageNotFoundComponents/ItemCard";
+import Message from "../../components/PageNotFoundComponents/Message";
 const PageNotFound = () => {
   return (
     <>
-      <Box>
-        <Icon>Icon</Icon>
-        <Typography variant="body1">
-          Sorry, we can't find the page your looking for
-        </Typography>
+      <Box
+        sx={{
+          textAlign: "center",
+          paddingBlock: 5,
+          background:
+            "linear-gradient(180deg, #0a99dc, rgba(7,200,239,1), rgba(255,255,255,1) 100%)",
+        }}
+      >
+        <Message />
       </Box>
-      <Box>
-        <Typography variant="body1">Let's get you back on track...</Typography>
-        <ItemCard />
-        {/* here will go some cards */}
-        {/* <List>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText primary="item"></ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </List> */}
+      <Box sx={{ paddingBlock: 4 }}>
+        <Typography variant="body1" textAlign="center" fontSize={28}>
+          Let's get you back on track...
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexGrow: "wrap",
+            width: "100%",
+            gap: 2,
+            paddingBlock: 2,
+          }}
+        >
+          <ItemCard />
+          <ItemCard />
+        </Box>
       </Box>
     </>
   );
