d229592fc82f97c01300536ce34b46ed1a44a9ea
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2024-04-11 13:10:19 +0300

    fixed hero section

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography/Typography";
+import HeroSection from "../../components/HeroSection/HeroSection";
 const Home = () => {
   return (
     <>
@@ -9,7 +8,7 @@ const Home = () => {
           backgroundColor: "#166894",
           width: "100vw",
           height: "100vh",
-          position: "absolute",
+          position: { xs: "absolute", md: "fixed" },
           top: 0,
           left: 0,
           zIndex: -1,
@@ -29,12 +28,9 @@ const Home = () => {
       </Box>
       <Box
         sx={{
-          position: "absolute",
           width: "100vw",
-          height: "100vh",
-          top: 0,
-          left: 0,
-          margin: 0,
+          background: "red",
+          margin: "auto 0",
           display: "grid",
           gridTemplateColumns: { md: "1.5fr 1fr" },
         }}
@@ -42,46 +38,16 @@ const Home = () => {
         <Box
           sx={{
             color: "white",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
             paddingInline: { xs: "5%", md: "10%" },
+            background: "green",
+            // height: "max(50vw, 100vh - 117.317px)",
           }}
         >
-          <Typography sx={{ fontSize: { xs: 54, md: 72 }, fontWeight: "bold" }}>
-            Raining Offers For Hot Summer!
-          </Typography>
-          <Typography sx={{ fontSize: 18, marginBlock: 4 }}>
-            25% Off On All Products
-          </Typography>
-          <Box>
-            <Button
-              color={"primary"}
-              variant="contained"
-              sx={{
-                borderRadius: "unset",
-                fontSize: 20,
-                marginRight: 2,
-                boxShadow: "unset",
-              }}
-            >
-              SHOP NOW
-            </Button>
-            <Button
-              variant="outlined"
-              sx={{
-                borderRadius: "unset",
-                fontSize: 20,
-                boxShadow: "unset",
-                border: "1px solid white",
-                color: "white",
-              }}
-            >
-              Find More
-            </Button>
-          </Box>
+          <HeroSection />
         </Box>
       </Box>
+
+      <Box>height</Box>
     </>
   );
 };
