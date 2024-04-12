1438b21c0e6c5b9f035947c884ada3fd4e18ef6e
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2024-04-12 15:50:50 +0300

    created product card component

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,6 +1,8 @@
 import Box from "@mui/material/Box";
 import HeroSection from "../../components/HomeComponents/HeroSection";
 import Background from "../../components/HomeComponents/Background";
+import { Grid, Typography } from "@mui/material";
+import ProductCard from "../../components/HomeComponents/ProductCard";
 const Home = () => {
   return (
     <>
@@ -11,7 +13,7 @@ const Home = () => {
           width: "100vw",
           margin: "auto 0",
           background:
-            "linear-gradient(to top,rgba(82, 91, 23, 0.25),rgba(0,0,0,0.3), rgba(0,0,0,0.3), rgba(255,255,255,0))",
+            "linear-gradient(to top,rgba(82, 91, 23, 0.25), rgba(0,0,0,0.3), rgba(255,255,255,0))",
           // height: "max(50vw, 100vh - 117.317px)",
         }}
       >
@@ -23,12 +25,42 @@ const Home = () => {
         sx={{
           background: "white",
           paddingBlock: "4rem",
-          paddingInline: "calc(18% - 2.5rem)",
+          paddingInline: "calc(9% - 1rem)",
           height: "auto",
           minHeight: "100vh",
         }}
       >
-        items
+        <Box>
+          <Typography
+            sx={{
+              textAlign: "center",
+              paddingBlock: "clamp(1rem, 8vw - 6rem, 2rem)",
+              borderBottom: "2px solid blue",
+              fontSize: "clamp(2rem, -1.6875rem + 8vw, 3rem)",
+            }}
+          >
+            Featured Products
+          </Typography>
+          {/* Products go here */}
+          <Box sx={{ marginBlock: 8, width: "100%" }}>
+            <Grid
+              container
+              spacing={2}
+              // sx={{ background: "red", padding: "unset" }}
+            >
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard />
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard /> <ProductCard /> <ProductCard />{" "}
+              <ProductCard /> <ProductCard />
+            </Grid>
+          </Box>
+        </Box>
       </Box>
     </>
   );
